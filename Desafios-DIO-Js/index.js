c9f5281fb220ee68eac1957f36fc1c251c174849
JackAzis <jackaziss@gmail.com>
2023-10-05 18:43:45 -0300

    Desafio classificador de Nivel de Herói

--- Desafios-DIO-Js/index.js
+++ Desafios-DIO-Js/index.js
@@ -1,7 +1,28 @@
-let heroi = dav
-let xp = 51
+let heroi = "Jack";
+let xp = 7000;
 
-
-
-
-console.log("O herói "+ heroi + " está no nivel de " + xp )
+if (xp <= 1000){
+    xp = "Ferro"
+}
+else if ((xp >= 1001) && (xp <= 2000)){
+    xp = "Bronze"
+}
+else if ((xp >= 2001) && (xp <= 5000)){
+    xp = "Prata"
+}
+else if ((xp >= 5001) && (xp <= 7000)){
+    xp = "Ouro"
+}
+else if ((xp >= 7001) && (xp <= 8000)){
+    xp = "Platina"
+}      
+else if ((xp >= 8001) && (xp <= 9000)){
+    xp = "Ascendente"
+}   
+else if ((xp >= 9001) && (xp <= 10000)){
+    xp = "Imortal"
+}   
+else if (xp > 10001){
+    xp = "Radiante"
+}       
+console.log("O herói "+ heroi + " está no nivel " + xp);
