62b310cc96f0b73ee60f71bc6b06976bbd146528
JackAzis <jackaziss@gmail.com>
2023-10-07 00:38:16 -0300

    Integração do desafio partidas rankeadas

---
+++ Desafios-DIO-Js/desafioPartidasRankeadas.js
@@ -0,0 +1,40 @@
+let vitorias = 100
+let derrotas = 5
+//let media = (vitorias - derrotas)
+//let partidas= (vitorias,)
+
+function subtracao() {
+    return vitorias - derrotas;  
+}
+
+let saldoVitorias = subtracao()
+if (saldoVitorias <=10){
+    nivel = "Ferro"
+}
+else if ((saldoVitorias >= 11) && (saldoVitorias <= 20)){
+    nivel = "Bronze"
+}
+else if ((saldoVitorias >= 21) && (saldoVitorias <= 50)){
+    nivel = "Prata"
+}
+else if ((saldoVitorias >= 51) && (saldoVitorias <= 80)){
+    nivel = "Ouro"
+}
+else if ((saldoVitorias >= 81) && (saldoVitorias <= 90)){
+    nivel = "Diamante"
+}
+else if ((saldoVitorias >= 91) && (saldoVitorias <= 100)){
+    nivel = "Lendario"
+}
+else if (saldoVitorias >= 101){
+    nivel = "Imortal"
+}
+console.log("O Herói tem de saldo de " + saldoVitorias + " está no nivel " + nivel)
+
+
+
+
+
+
+// if (media <= 10)
+//  " está no nivel "+nivel 
